ae073e4225a03e2215a916f9901c91a6eaaf992c
ADENIYI IFE <110723807+ifecsoftware@users.noreply.github.com>
2022-09-25 18:43:00 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+
 /**
- *main - entry point
- *
- *Return: 0 after printing the function
- */
+ *  main - Prints String
+ *  Description: "\"Programming is like building a multilingual puzzle"
+ *  Return: 0
+**/
+
 int main(void)
+
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
 }
