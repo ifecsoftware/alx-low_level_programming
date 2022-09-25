70d75202409f0ba0ab0f5f45545ac54598931659
ADENIYI IFE <110723807+ifecsoftware@users.noreply.github.com>
2022-09-25 18:54:21 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,11 @@
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
+
 /**
- * main - prints the standrad error sandwiched around the  standard outpu/t
- *
- * Return: returns 1
+ * main - Entry point
+ * Return: return 1
  */
+
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
