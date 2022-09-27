04b23a7c5e1bfc3d7d5b9b50b625d94501474075
ADENIYI IFE <110723807+ifecsoftware@users.noreply.github.com>
2022-09-27 09:55:12 +0100

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,43 +1,31 @@
 #include "main.h"
+
 /**
- * _strstr - function that locates a substring.
- *@haystack: first value -char
- *@needle: second value - char
- *
- * Return: char with result
- */
+ * _strstr - finds the first occurrence of the substring.
+ * needle in the string haystack.
+ * @haystack: entire string.
+ * @needle: substring.
+ * Return: pointer to the beginning of located substring or
+ * NULL if the substring is not found.
+*/
 char *_strstr(char *haystack, char *needle)
 {
-	int count = 0, a = 0, k, j = 0, l, i = 0;
-	char *p;
+	char *bhaystack;
+	char *pneedle;
 
-	while (needle[count] != '\0')
+	while (*haystack != '\0')
 	{
-		count++;
-	}
-	while (haystack[i] != '\0')
-	{
-		i++;
-			}
-	if (count == 0)
-		return (haystack);
-	while (haystack[j] != '\0')
-	{
-		if (haystack[j] == needle[0])
+		bhaystack = haystack;
+		pneedle = needle;
+
+		while (*haystack != '\0' && *pneedle != '\0' && *haystack == *pneedle)
 		{
-			p = &haystack[j];
-			l = j;
-			a = 0;
-			for (k = 0; k < count; k++)
-			{
-				if (haystack[l] == needle[k])
-					a++;
-				l++;
-			}
+			haystack++;
+			pneedle++;
 		}
-		if (a == count)
-			return (p);
-		j++;
+		if (!*pneedle)
+			return (bhaystack);
+		haystack = bhaystack + 1;
 	}
-	return ('\0');
+	return (0);
 }
