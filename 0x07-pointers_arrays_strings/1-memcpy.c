60f443d336f46cada8621c9c22473f1201dec13c
ADENIYI IFE <110723807+ifecsoftware@users.noreply.github.com>
2022-09-27 09:23:02 +0100

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,19 +1,17 @@
 #include "main.h"
 /**
- * _memcpy - function that copies memory area
- *@dest: the address of memory to print
- *@src: second value
- *@n: thirth value
- *
- * Return: char with result of memset
+ * _memcpy - copies memory area,
+ * @dest: destination memory area.
+ * @src: source memory area.
+ * @n: bytes filled.
+ * Return: the pointer to dest.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-	{
-		dest[i] = src[i];
-	}
+		*(dest + i) =  *(src + i);
+
 	return (dest);
 }
