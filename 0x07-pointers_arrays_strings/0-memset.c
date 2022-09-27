2638218df501001709132b9e90aa7b3ab279dfc3
ADENIYI IFE <110723807+ifecsoftware@users.noreply.github.com>
2022-09-27 09:12:37 +0100

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,16 +1,17 @@
+#include "main.h"
 /**
- *_memset - function declaration
- *@s: First parameter
- *@b: second parameter
- *@n: third parameter
- *
- *Return: always 0
+ * _memset - fills memory with a constant byte,
+ * @s: memory area.
+ * @b: constant byte.
+ * @n: bytes filled.
+ * Return: the pointer to dest.
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i = 0;
-for (i = 0; i < n; i++)
-s[i] = b;
-return (s);
+	      unsigned int i;
+
+	      for (i = 0; i < n; i++)
+	    	*(s + i) =  b;
+
+       	return (s);
 }
