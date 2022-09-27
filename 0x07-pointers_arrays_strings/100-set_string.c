db0159cf63ed0194babb5250dad93a9750954357
ADENIYI IFE <110723807+ifecsoftware@users.noreply.github.com>
2022-09-27 09:59:36 +0100

    Update 100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,11 +1,12 @@
 #include "main.h"
+
 /**
- * set_string - function that  sets the value of a pointer to a char.
- *@s: first value -char
- *@to: second value -int
+ * set_string - sets the value of a pointer to a char
+ * @s: source adress.
+ * @to: target adress.
+ * Return: no return.
  */
 void set_string(char **s, char *to)
 {
-
 	*s = to;
 }
